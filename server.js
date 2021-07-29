0edc86080967c90e81c37e5d52b4c0dec6ac2996
jawad ali <024jawadali@gmail.com>
2021-07-29 10:00:40 +0500

    version 2

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use(express.static("./build"));
 //     res.json("works now")
 // })
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.dirname(__dirname, "build", "index.html"));
+    res.sendFile(path.dirname(__dirname, "client", "build", "index.html"));
   });
 }
 
